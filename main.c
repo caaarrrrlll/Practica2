77c0c3ebf8e6c255879120fae68f3ed2a39a098f
caaarrrrlll <133277213+caaarrrrlll@users.noreply.github.com>
2023-07-06 15:48:16 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+
+    char c1,c2,c3;
+    
+    printf("Ingrese 3 caracteres: \n");
+    scanf("%c %c %c", &c1, &c2, &c3);
+
+    printf("%c-%c-%c son los caracteres introducidos", c1,c2,c3);    
+}
